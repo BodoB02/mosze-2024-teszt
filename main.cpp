4f5efbebd36163c7e6d9e5d0aa18b131790c98fd
Bálint Bodó <bobalint2003@gmail.com>
2024-10-02 12:48:37 +0200

    Kommentezett

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa' for (int i = 0;)
+    int *b = new int[NELEMENTS];                            // Helytelen hivatkozás és kibővítjük a tömböt
+    std::cout << '1-100 ertekek duplazasa' for (int i = 0;) // Hiányzó pontosvessző és kiegészítjök a ciklus követelményeit
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // Kiegészítjök a ciklus követelményeit
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // Hiányzó pontosvessző és az értékek kimutatása
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;                           // Inancializáció
+    for (int i = 0; i < N_ELEMENTS, i++) // Hiányzó pontosvessző
     {
-        atlag += b[i]
+        atlag += b[i] // Hiányzó pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
