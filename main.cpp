2494e62fd75e1d6e8582fc67787ed6729abc4db4
Bálint Bodó <bobalint2003@gmail.com>
2024-10-02 12:54:13 +0200

    Kész

--- main.cpp
+++ main.cpp
@@ -4,20 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];                            // Helytelen hivatkozás és kibővítjük a tömböt
-    std::cout << '1-100 ertekek duplazasa' for (int i = 0;) // Hiányzó pontosvessző és kiegészítjök a ciklus követelményeit
+    int *b = new int[N_ELEMENTS + 1];                    // Helytelen hivatkozás és kibővítjük a tömböt
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // Hiányzó pontosvessző
+    for (int i = 1; i <= N_ELEMENTS; i++)                // Kiegészítjök a ciklus követelményeit
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // Kiegészítjök a ciklus követelményeit
+    for (int i = 1; i <= N_ELEMENTS + 1; i++) // Kiegészítjök a ciklus követelményeit
     {
-        std::cout << "Ertek:" // Hiányzó pontosvessző és az értékek kimutatása
+        std::cout << i << ". Ertek: " << b[i] << std::endl; // Hiányzó pontosvessző és az értékek kimutatása
     }
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;                           // Inancializáció
-    for (int i = 0; i < N_ELEMENTS, i++) // Hiányzó pontosvessző
+    std::cout << "Atlag szamitasa: ";
+    int atlag = 0;                            // Inancializáció
+    for (int i = 1; i <= N_ELEMENTS + 1; i++) // Hiányzó pontosvessző és ciklus követelményeinek kiegészítése
     {
-        atlag += b[i] // Hiányzó pontosvessző
+        atlag += b[i]; // Hiányzó pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
